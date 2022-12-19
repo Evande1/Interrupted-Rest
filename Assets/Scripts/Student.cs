37b01804bddd988aa584f36a2c101d330a20a098
Seah Ying Xiang <seahyx@gmail.com>
2022-12-19 14:46:13 +0800

    Setup basic scripts

---
+++ Assets/Scripts/Student.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Student : MonoBehaviour
+{
+	#region Serializables
+
+	[Header("Tooltips")]
+
+	[SerializeField]
+	private Animator animator;
+	[SerializeField]
+	private AudioSource audioSource;
+
+	#endregion
+
+	#region Monobehaviour
+
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	#endregion
+
+	#region Student Functions
+
+
+
+	#endregion
+}
